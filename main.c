bb337d2ddb50691cacab25e3b14603dd2ff07e26
SuperPou <superpou1filip@gmail.com>
2025-10-08 00:35:16 +0200

    clean up code

--- main.c
+++ main.c
@@ -72,73 +72,50 @@ size_t write_to_buffer(void *buffer, size_t size, size_t nmemb, void *userp) {
 	return total;
 }
 
-int main(int argc, char *argv[]) {
-	int ch;
-	int action = 0;
-	int setserver = 0;
-	char server[512];
-	char confpath[512];
-	struct json_object *commandjson = json_object_new_object();;
+int sendrq(struct json_object *requestjson, char *server, struct json_object **responsejson) {
+	struct Buffer responsebuff = {{0}, 0};
+	enum json_tokener_error responsestate;
+
+	CURL *handle = curl_easy_init();
+
+	curl_easy_setopt(handle, CURLOPT_URL, server);
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_to_buffer);
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, &responsebuff.data);
+
+	printf("Request JSON: %s\n", json_object_to_json_string(requestjson)); // debug
+	curl_easy_setopt(handle, CURLOPT_POSTFIELDS, json_object_to_json_string(requestjson));
+	curl_easy_perform(handle);
+	*responsejson = json_tokener_parse_verbose(responsebuff.data, &responsestate);
+	if (responsestate > json_tokener_continue) { // anything higher than json_tokener_continue is an error
+		return 1;
+	}
+	json_object_object_add(*responsejson, "Server", json_object_new_string(server)); // add the server to the returned response for debug and to write it to config
+	printf("Response: %s\n",json_object_to_json_string(*responsejson));
+
+	return 0;
+}
+
+int ac_account(int ac, char *server, char **token) {
+	struct json_object *requestjson = json_object_new_object();
 	struct json_object *responsejson;
 	struct json_object *confjson;
-	enum json_tokener_error responsestate;
 	enum json_tokener_error confstate;
-	// get options
-	while ((ch = getopt_long(argc, argv, "s:c:lm", long_options, NULL)) != -1) {
-		switch (ch) {
-			case 'l':
-				action = 1;
-				break;
-			case 'c':
-				json_object_object_add(commandjson, "Command", json_object_new_string(optarg));
-				break;
-			case 's':
-				strcpy(server, optarg);
-				setserver = 1;
-				break;
-			case 'm':
-				action = 2;
-				break;
-			default:
-				fprintf(stderr, "Unknown option\n");
-				return 1;
-		}
-	}
+	char confpath[512];
+
+	/* ac 0 = load config
+	 * ac 1 = log in
+	 * ac 2 = register
+	 * ac 3 = log out
+	 */
 
 	strcpy(confpath, getenv("HOME"));
 	strcat(confpath,"/.config/communicator/");
 
-	// initialize curl
-	curl_global_init(CURL_GLOBAL_ALL);
-	CURL *handle = curl_easy_init();
-
 	mkdir_p(confpath);
 	strcat(confpath,"login.json"); // append the file to the path after creating the directory
-
 	FILE *file = fopen(confpath, "r+");
-	if (file && argc < 2) { printf("%s", HELP_MSG); return 1; }
-	if (!file || action == 1) {
-		file = fopen(confpath, "wb");
-		if (!file) { perror("Something went wrong creating config\n"); return 1;};
-		char confstr[512];
-		char login[MAX_LOGIN_LEN];
-		char pass[MAX_PASS_LEN];
-
-		action = 1;
-
-		if (!setserver) { printf("Please specify a server with -s or --server\n"); return 1;}
-		printf("Please log in\n");
-		printf("Login: ");
-		fgets(login, MAX_LOGIN_LEN, stdin);
-		printf("Password: ");
-		fgets(pass, MAX_PASS_LEN, stdin);
-		login[strcspn(login, "\n")] = 0;
-		pass[strcspn(pass, "\n")] = 0;
-
-		json_object_object_add(commandjson, "Username", json_object_new_string(login));
-		json_object_object_add(commandjson, "Password", json_object_new_string(pass));
-		json_object_object_add(commandjson, "Command", json_object_new_string("login"));
-	} else {
+	if (ac == 0 || ac >= 3) {
+		if (!file) {printf("Please log in first.\n"); return 1;}
 		// read config from file
 		fseek(file, 0, SEEK_END);
 		long filesize = ftell(file);
@@ -150,17 +127,84 @@ int main(int argc, char *argv[]) {
 
 		confjson = json_tokener_parse_verbose(confstr, &confstate);
 		if (confstate > json_tokener_continue) {
-			printf("Error reading config, please try to log in again using -l or --login\n");
+			printf("Error reading config, please try to log in again.\n");
 			return 1;
 		}
+		strcpy(*token, json_object_get_string(json_object_object_get(confjson, "Token")));
+		strcpy(server, json_object_get_string(json_object_object_get(confjson, "Server")));
+		return 0;
+	} else {
+		file = fopen(confpath, "wb");
+		if (!file) { perror("Something went wrong creating config\n"); return 1;};
+	}
 
-		if (!setserver) strcpy(server, json_object_get_string(json_object_object_get(confjson, "Server")));
-		json_object_object_add(commandjson, "Token", json_object_object_get(confjson, "Token"));
+	char login[MAX_LOGIN_LEN];
+	char pass[MAX_PASS_LEN];
 
+	printf("Login: ");
+	fgets(login, MAX_LOGIN_LEN, stdin);
+	printf("Password: ");
+	fgets(pass, MAX_PASS_LEN, stdin);
+	login[strcspn(login, "\n")] = 0;
+	pass[strcspn(pass, "\n")] = 0;
+
+	json_object_object_add(requestjson, "Username", json_object_new_string(login));
+	json_object_object_add(requestjson, "Password", json_object_new_string(pass));
+	json_object_object_add(requestjson, "Command", json_object_new_string("login"));
+	if (sendrq(requestjson, server, &responsejson)) {
+		printf("Something went wrong while logging in, please check connection\n");
+	}
+	const char *responsestr = json_object_to_json_string(responsejson);
+	fwrite(responsestr, 1, strlen(responsestr), file);
+	fclose(file);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	int ch;
+	int action = 0;
+	char *server = malloc(512);
+	char *token = malloc(33);
+	struct json_object *requestjson = json_object_new_object();
+	struct json_object *responsejson;
+	// get options
+	while ((ch = getopt_long(argc, argv, "s:c:lm", long_options, NULL)) != -1) {
+		switch (ch) {
+			case 'l':
+				action = 1;
+				break;
+			case 'c':
+				action = 4;
+				json_object_object_add(requestjson, "Command", json_object_new_string(optarg));
+				break;
+			case 's':
+				strcpy(server, optarg);
+				break;
+			case 'm':
+				action = 5;
+				break;
+			default:
+				fprintf(stderr, "Unknown option\n");
+				return 1;
+		}
+	}
+
+	// initialize curl
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	if(action > 3) {
+		ac_account(0, server, &token);
+		json_object_object_add(requestjson, "Token", json_object_new_string(token));
+	} else if (action > 0) {
+		ac_account(action, server, &token);
+		return 0;
+	} else {
+		printf("%s\n", HELP_MSG);
+		return 1;
 	}
 
 	switch(action) {
-		case 2:
+		case 5:
 			char msg[MSG_SIZE];
 			char recipient[MAX_LOGIN_LEN];
 			printf("Recipient: ");
@@ -169,32 +213,15 @@ int main(int argc, char *argv[]) {
 			fgets(msg, MSG_SIZE, stdin);
 			recipient[strcspn(recipient, "\n")] = 0;
 			msg[strcspn(msg, "\n")] = 0;
-			json_object_object_add(commandjson, "Command", json_object_new_string("send"));
-			json_object_object_add(commandjson, "Message", json_object_new_string(msg));
-			json_object_object_add(commandjson, "MessageUser", json_object_new_string(recipient));
+			json_object_object_add(requestjson, "Command", json_object_new_string("send"));
+			json_object_object_add(requestjson, "Message", json_object_new_string(msg));
+			json_object_object_add(requestjson, "MessageUser", json_object_new_string(recipient));
 			break;
 	}
 
-	struct Buffer response = {{0}, 0};
-
-	curl_easy_setopt(handle, CURLOPT_URL, server);
-	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_to_buffer);
-	curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response.data);
-
-	printf("Request JSON: %s\n", json_object_to_json_string(commandjson)); // debug
-	curl_easy_setopt(handle, CURLOPT_POSTFIELDS, json_object_to_json_string(commandjson));
-	curl_easy_perform(handle);
-
-	responsejson = json_tokener_parse_verbose(response.data, &responsestate);
-	if (responsestate > json_tokener_continue) { // anything higher than json_tokener_continue is an error
-		printf("Something went wrong while parsing response JSON\n");
+	if (sendrq(requestjson, server, &responsejson)) {
+		printf("Server: %s\n", server);
+		printf("Something went wrong sending request, please check connection\n");
 		return 1;
 	}
-
-	json_object_object_add(responsejson, "Server", json_object_new_string(server)); // add the server to the returned request to write it to config
-	printf("Welcome to communicator!\n%s\n", json_object_to_json_string_ext(responsejson, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY)); // debug
-	const char *responsestr = json_object_to_json_string(responsejson);
-	if (action == 1 && responsestate < json_tokener_error_depth)
-		fwrite(responsestr, 1, strlen(responsestr), file);
-	fclose(file);
 }
